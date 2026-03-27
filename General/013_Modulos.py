8501f856a860cc14e66533a23a5629bb5adb5b02
JohanSebastianPZ <johansebastian627@gmail.com>
2026-03-27 12:41:32 +0100

    Practica import funciones

---
+++ General/013_Modulos.py
@@ -0,0 +1,4 @@
+# import 010_Funciones - En python los archivos no se pueden importa si COMIENZAN por un numero
+import modulos
+# import "../Actividades/actividad_listas2"
+modulos.sumar_dos_valores_con_tipo(12,12)
---
+++ General/modulos.py
@@ -0,0 +1,5 @@
+def sumar_dos_valores_con_tipo(primer_numero: int, segundo_numero: int):
+    print(primer_numero + segundo_numero)
+    
+
+
